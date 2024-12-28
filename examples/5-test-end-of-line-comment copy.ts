e876b323b4dd877fe12fb16c29c26b4f4d758b70
pflanagan <pflanagan@grubhub.com>
2024-12-28 18:57:39 +0800

    leave note

--- examples/5-test-end-of-line-comment copy.ts
+++ examples/5-test-end-of-line-comment copy.ts
@@ -2,4 +2,5 @@
 // edit this file to see if the test line gets commented on the PR
 // edit: 6:00 PM Dec 28, 2024
 
+// TODO: this is broken
 console.log('Comment at the end of a line') // MERGE: merge reminders at the end of lines will not include the code from that line
