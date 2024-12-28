51c8fec4ee6ee023bb00d549b4a0d0c7ba05c26f
pflanagan <pflanagan@grubhub.com>
2024-12-28 18:54:51 +0800

    test main
    leave note
    
    remove cope from file name
    
    customization
    
    customization

--- examples/7-custom-flag.ts
+++ examples/7-custom-flag.ts
@@ -2,4 +2,4 @@
 // edit this file to see if the test line gets commented on the PR
 // edit: 6:00 PM Dec 28, 2024
 
-// MERGE: merge reminders on edited code will be commented on the PR
+// @merge: This is a custom flag that will be commented on the PR
